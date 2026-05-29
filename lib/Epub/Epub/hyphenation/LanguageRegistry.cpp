8eae9bf298cf5ace93c09cbd2f23847f7e0088d6
Fernando Meyer <fm@pobox.com>
2026-05-29 15:31:42 +0200

    feat: Add Portuguese hyphenation support

--- lib/Epub/Epub/hyphenation/LanguageRegistry.cpp
+++ lib/Epub/Epub/hyphenation/LanguageRegistry.cpp
@@ -11,6 +11,7 @@
 #include "generated/hyph-fr.trie.h"
 #include "generated/hyph-it.trie.h"
 #include "generated/hyph-pl.trie.h"
+#include "generated/hyph-pt.trie.h"
 #include "generated/hyph-ru.trie.h"
 #include "generated/hyph-sv.trie.h"
 #include "generated/hyph-uk.trie.h"
@@ -28,8 +29,9 @@ LanguageHyphenator swedishHyphenator(sv_patterns, isLatinLetter, toLowerLatin);
 LanguageHyphenator ukrainianHyphenator(uk_patterns, isCyrillicLetter, toLowerCyrillic);
 LanguageHyphenator polishHyphenator(pl_patterns, isLatinLetter, toLowerLatin);
 LanguageHyphenator finnishHyphenator(fi_patterns, isLatinLetter, toLowerLatin);
+LanguageHyphenator portugueseHyphenator(pt_patterns, isLatinLetter, toLowerLatin);
 
-using EntryArray = std::array<LanguageEntry, 10>;
+using EntryArray = std::array<LanguageEntry, 11>;
 
 const EntryArray& entries() {
   static const EntryArray kEntries = {{{"english", "en", &englishHyphenator},
@@ -39,6 +41,7 @@ const EntryArray& entries() {
                                        {"spanish", "es", &spanishHyphenator},
                                        {"italian", "it", &italianHyphenator},
                                        {"polish", "pl", &polishHyphenator},
+                                       {"portuguese", "pt", &portugueseHyphenator},
                                        {"swedish", "sv", &swedishHyphenator},
                                        {"ukrainian", "uk", &ukrainianHyphenator},
                                        {"finnish", "fi", &finnishHyphenator}}};
